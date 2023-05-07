05d540418eab7e2d2f400a27fb99016851026ef9
siemens <sema.perstnev.01@mail.ru>
2023-05-07 17:02:39 +0300

    commit1

---
+++ src/test/java/com/example/onlineshopdemo/OnlineshopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
